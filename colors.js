4257bd1d7f2bbf05bc8b11a30a13ea9aa136a856
TR Jordan <terral.jordan@gmail.com>
2012-02-08 01:08:45 +0000

    Abstract out interpolation.

--- colors.js
+++ colors.js
@@ -1,41 +1,44 @@
+
+// Generalized interpolation
+function getDrawFunction(prop1, prop2, prop3) {
+    var props = Array.prototype.slice.call(arguments);
+    var drawFunc = function(canvas, start, end) {
+        var s = Color(start);
+        var e = Color(end);
+
+        var scales = props.map(function(prop) {
+            return pv.Scale.linear(0, 1).range(s[prop](), e[prop]());
+        });
+
+        var vis = new pv.Panel()
+            .canvas(canvas.get(0))
+            .add(pv.Bar)
+            .data(pv.range(0, 1, 1/canvas.width()))
+            .left(function() { return this.index; })
+            .fillStyle(function(d) {
+                var c = Color();
+                _.zip(props, scales).map(function(ps) {
+                    c = c[ps[0]](ps[1](d));
+                });
+                return pv.color(c.rgbString());
+            });
+
+        vis.render();
+    };
+
+    return drawFunc;
+}
+
 // RGB
-
 function drawRgb(canvas, start, end) {
-    var scale = pv.Scale.linear(0, 1).range(pv.color(start), pv.color(end));
-
-    var vis = new pv.Panel()
-        .canvas(canvas.get(0))
-      .add(pv.Bar)
-        .data(pv.range(0, 1, 1/canvas.width()))
-        .left(function() { return this.index; })
-        .fillStyle(scale);
-
-    vis.render();
+    var f = getDrawFunction('red', 'green', 'blue');
+    f.apply(this, arguments);
 }
 
 // HSL
 function drawHsl(canvas, start, end) {
-    var start = Color(start);
-    var end = Color(end);
-
-    var hueScale = pv.Scale.linear(0, 1).range(start.hue(), end.hue());
-    var saturationScale = pv.Scale.linear(0, 1).range(start.saturation(), end.saturation());
-    var lightnessScale = pv.Scale.linear(0, 1).range(start.lightness(), end.lightness());
-
-    var vis = new pv.Panel()
-        .canvas(canvas.get(0))
-      .add(pv.Bar)
-        .data(pv.range(0, 1, 1/canvas.width()))
-        .left(function() { return this.index; })
-        .fillStyle(function(d) {
-            var c = Color()
-                .hue(hueScale(d))
-                .saturation(saturationScale(d))
-                .lightness(lightnessScale(d));
-            return pv.color(c.rgbString());
-        });
-
-    vis.render();
+    var f = getDrawFunction('hue', 'saturation', 'lightness');
+    f.apply(this, arguments);
 }
 
 // Main doc handler
